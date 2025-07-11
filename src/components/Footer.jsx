cae69a23af7f7a946a874706bf7912d1bd58432f
kayleesatch <kayleesatch@gmail.com>
2025-07-10 19:54:38 -0600

    edited the footer to make center

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
 export default function Footer() {
     return (
         <footer className="bg-red-900 text-yellow-50 text-sm">
-            <div className="max-w-6xl mx-auto px-4 flex items-center justify-between">
+            <div className="max-w-6xl mx-auto px-2 flex items-center justify-between">
                 <img 
                     src="/Hidden Cafe-Logo(3).png" 
-                    className="w-auto h-20" 
+                    className="h-15 w-auto" 
                     alt="Logo" 
                 />
 
-                <div className="text-center flex-1">
+                <div className="text-center items-center flex-1">
                     <p>Made with 🧡 by Kaylynn Satchell</p>
                     <div className="mt-1 text-xs opacity-30 hover:opacity-100 transition">
                         <a href="/login" className="underline">Staff Login</a>
